825b5aa1a214a9a8e4ef5aa0166de63e2b7c8062
Dimitri Capitaine <dimitri.capitaine@ovhcloud.com>
2020-09-10 21:44:03 +0200

    Add ParentID for message

--- sdk/messages_api.go
+++ sdk/messages_api.go
@@ -16,6 +16,7 @@ type MessagesService service
 // MessageCreateRequest is the Create Message Request Parameters
 type MessageCreateRequest struct {
 	RoomID        string   `json:"roomId,omitempty"`        // Room ID.
+	ParentID      string   `json:"parentId,omitempty"`      // Parent ID.
 	ToPersonID    string   `json:"toPersonId,omitempty"`    // Person ID (for type=direct).
 	ToPersonEmail string   `json:"toPersonEmail,omitempty"` // Person email (for type=direct).
 	Text          string   `json:"text,omitempty"`          // Message in plain text format.
